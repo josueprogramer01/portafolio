ce3eb20f38ebaa772e452f25807db5f4f81a6f0f
josueprogramer01 <josue-chiva99@hotmail.com>
2025-02-24 17:54:29 -0600

    se arreglo el navbar para dispositivos moviles

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 import "./styles/navBar.css";
 
 const NavBar = (props) => {
 	const { active } = props;
+	const [menuOpen, setMenuOpen] = useState(false);
+
+	const handleMenu = () => setMenuOpen(!menuOpen);
+	const closeMenu = () => setMenuOpen(false);
 
 	return (
 		<React.Fragment>
 			<div className="nav-container">
 				<nav className="navbar">
-					<div className="nav-background">
-						<ul className="nav-list">
+					<div className="nav-background" onClick={handleMenu}>
+						<div className="nav-header">
+							<div className="nav-hamburger" onClick={handleMenu}>
+								<FontAwesomeIcon icon={menuOpen ? faTimes : faBars}	/>
+							</div>
+						</div>
+						<ul className={`nav-list ${menuOpen ? "open" : ""}`}>
 							<li
 								className={
 									active === "home"
 										? "nav-item active"
 										: "nav-item"
 								}
+								onClick={closeMenu}
 							>
 								<Link to="/">Inicio</Link>
 							</li>
@@ -27,6 +39,7 @@ const NavBar = (props) => {
 										? "nav-item active"
 										: "nav-item"
 								}
+								onClick={closeMenu}
 							>
 								<Link to="/about">Sobre Mí</Link>
 							</li>
@@ -36,6 +49,7 @@ const NavBar = (props) => {
 										? "nav-item active"
 										: "nav-item"
 								}
+								onClick={closeMenu}
 							>
 								<Link to="/projects">Proyectos</Link>
 							</li>
@@ -45,6 +59,7 @@ const NavBar = (props) => {
 										? "nav-item active"
 										: "nav-item"
 								}
+								onClick={closeMenu}
 							>
 								<Link to="/articles">Articulos</Link>
 							</li>
@@ -54,6 +69,7 @@ const NavBar = (props) => {
 										? "nav-item active"
 										: "nav-item"
 								}
+								onClick={closeMenu}
 							>
 								<Link to="/contact">Contacto</Link>
 							</li>
@@ -63,6 +79,7 @@ const NavBar = (props) => {
 										? "nav-item active"
 										: "nav-item"
 								}
+								onClick={closeMenu}
 							>
 								<Link to="/cv">CV</Link>
 							</li>
