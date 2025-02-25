b7ceab1ffc2733aeb417d9d6641ffabef172ae2d
josueprogramer01 <josue-chiva99@hotmail.com>
2025-02-25 11:18:46 -0600

    se añadio un toltip y se cambio la fuente de la app

--- src/components/dark/dark.jsx
+++ src/components/dark/dark.jsx
@@ -1,23 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
 import "./styles/dark.css";
 
 const DarkMode = () => {
-    const [darkMode, setDarkMode] = useState(() => {
-        return localStorage.getItem("darkMode") === "true";
-    });
+	const [darkMode, setDarkMode] = useState(() => {
+		return localStorage.getItem("darkMode") === "true";
+	});
 
-    useEffect(() => {
-        document.body.classList.toggle("dark-mode", darkMode);
-        localStorage.setItem("darkMode", darkMode);
-    }, [darkMode]);
+	useEffect(() => {
+		document.body.classList.toggle("dark-mode", darkMode);
+		localStorage.setItem("darkMode", darkMode);
+	}, [darkMode]);
 
-    return (
-        <button className="floating-btn" onClick={() => setDarkMode(!darkMode)}>
+	return (
+        <Tippy content={darkMode ? "Modo claro" : "Modo oscuro"} placement="left">
+          <button
+            className="floating-btn"
+            onClick={() => setDarkMode(!darkMode)}
+          >
             <FontAwesomeIcon icon={darkMode ? faSun : faMoon} />
-        </button>
-    );
-};
+          </button>
+        </Tippy>
+      );
+    };
 
 export default DarkMode;
